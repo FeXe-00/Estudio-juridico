ff33e83f2b0a7fa8c7934b92c2afca31208afb05
Fexe-00 <fsosaescalada@gmail.com>
2022-03-15 21:17:32 -0300

    levantando server

--- servidor.js
+++ servidor.js
@@ -1,9 +1,24 @@
-const math = require('./math')
-const http = require('http');
+const api_1 = require('./api_1');
+const express = require('express');
+
+
+const app = express();
+const port = 5000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: }))
+
+api_1(app);
+
+app.listen(port, () => {
+    console.log('localhost:'+port)
+})
+
+/* const http = require('http'); */
 
 /* console.log(math.add(25,8)); */
 
-const hostname = '127.0.0.1';
+/* const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) =>{
@@ -15,4 +30,4 @@ const server = http.createServer((req, res) =>{
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
-})
+}) */
